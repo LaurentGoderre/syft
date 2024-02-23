11d070018961f408b85cb79e72bf0600d3cc7d86
Laurent Goderre <laurent.goderre@docker.com>
2024-02-23 12:54:47 -0500

    Lua: Skip build sections in Rockspec files

--- syft/pkg/cataloger/lua/rockspec_parser.go
+++ syft/pkg/cataloger/lua/rockspec_parser.go
@@ -141,6 +141,15 @@ func parseRockspecNode(data []byte, i *int, locals map[string]string) (*rockspec
 
 	*i++
 	parsing.SkipWhitespace(data, i)
+
+	if key == "build" {
+		skipBuildNode(data, i)
+
+		return &rockspecNode{
+			key: ",",
+		}, nil
+	}
+
 	c = data[*i]
 
 	switch c {
@@ -415,6 +424,27 @@ func parseLocal(data []byte, i *int, locals map[string]string) error {
 	}
 
 	return nil
+}
+
+func skipBuildNode(data []byte, i *int) {
+	bracesCount := 0
+
+	for *i < len(data) {
+		c := data[*i]
+
+		switch c {
+		case '{':
+			bracesCount++
+		case '}':
+			bracesCount--
+		}
+
+		if bracesCount == 0 {
+			return
+		}
+
+		*i++
+	}
 }
 
 func skipExpression(data []byte, i *int) {
